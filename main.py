c2a238fff34134b49e085472ba9b5c9552989269
Francisquet Vargas <fvs00725@gmail.com>
2022-06-23 17:40:29 -0500

    added player play functionality

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def show_board(board):
             print(item, end=" ")
         print()
         
+def player_play(board, character):
+    location_row = int(input("What row would you like to play?: "))
+    location_col = int(input("What colum would you like to play?: "))
+    board[location_row][location_col] = character
+    
+    
+        
 board = []
 
 for _ in range(3):
@@ -23,5 +30,18 @@ for _ in range(3):
     for _ in range(3):
         row.append("-")
     board.append(row)
+    
+player_name = input("What is your name?: ")
+welcome_player(player_name)
+
+player_character = player_choose_character()
+
+show_board(board)
+
+player_play(board, player_character)
+
+show_board(board)
+
+
 
     
