66545850834117f133566bab84c33b6cc3afb34d
Francisquet Vargas <fvs00725@gmail.com>
2022-06-27 11:04:43 -0500

    added cpu functionality and ending game condition

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def player_choose_character():
         return "X"
     elif choice.upper() == "O":
         return "O"
+    else:
+        print("Invalid Character, choose between 'X' or 'O'")
+        player_choose_character()
     
 def show_board(board):
     steps = 2
@@ -19,36 +22,75 @@ def show_board(board):
         else:
             print(board[index], end=" ")
         
-def player_play(board, character, choice):
+def play(board, character, choice):
     board[choice - 1] = character
     
 def player_choice():
     choice = int(input("What position would you like to play?: "))
     if choice < 0 or choice > 9:
-        print("Please Enter a number between 0 and 2")
+        print("Please Enter a number between 1 and 9")
         player_choice()
     else:
         return choice  
+    
+def comp_character(player_character):
+    if player_character == "X":
+        return "O"
+    elif player_character == "O":
+        return "X"
+    
+def comp_choice():
+    comp_choice = random.randint(1, 9)
+    return comp_choice
+
+def is_board_full(board):
+    count = 0
+    for item in board:
+        if item == "O" or item == "X":
+            count += 1
+        else:
+            count = count
+    if count >= 9:
+        return True
+    else: 
+        return False
+            
+    
     
 board = []
 
 for index in range(9):
     board.append(index + 1)
-
     
+is_game_over = False
+
 player_name = input("What is your name?: ")
 welcome_player(player_name)
-
+print("-------------------------------------------------")
 player_character = player_choose_character()
 
-show_board(board)
+while is_game_over == False:
+    print("-------------------------------------------------")
+    show_board(board)
+    print("-------------------------------------------------")
+    player_choice_position = player_choice()
 
-player_choice_position = player_choice()
 
+    play(board, player_character, player_choice_position)
 
-player_play(board, player_character, player_choice_position)
+    show_board(board)
+    print("-------------------------------------------------")
+    cpu_character = comp_character(player_character)
 
-show_board(board)
+    cpu_choice_position = comp_choice()
+
+    play(board, cpu_character, cpu_choice_position)
+
+    show_board(board)
+    print("-------------------------------------------------")
+    
+    if is_board_full(board):
+        is_game_over = True
 
 
 
