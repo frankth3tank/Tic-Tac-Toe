b49ba3be864b78031d1f23dab8f76a5edd97c4f5
Francisquet Vargas <fvs00725@gmail.com>
2022-07-10 14:14:38 -0500

    change name of file to game logic and added some functionalities

---
+++ game_logic.py
@@ -0,0 +1,42 @@
+class GameLogic:
+    
+    def __init__(self):
+        pass
+    
+    def ending_condition(self, board, character, player):
+        if board[0] == character and board[1] == character and board[2] == character:
+            print(f"{player} Won!!")
+            return True
+        elif board[3] == character and board[4] == character and board[5] == character:
+            print(f"{player} Won!")
+            return True
+        elif board[6] == character and board[7] == character and board[8] == character:
+            print(f"{player} Won!")
+            return True
+        elif board[0] == character and board[3] == character and board[6] == character:
+            print(f"{player} Won!")
+            return True
+        elif board[1] == character and board[4] == character and board[7] == character:
+            print(f"{player} Won!")
+            return True
+        elif board[2] == character and board[5] == character and board[8] == character:
+            print(f"{player} Won!")
+            return True
+        elif board[0] == character and board[4] == character and board[8] == character:
+            print(f"{player} Won!")
+            return True
+        elif board[2] == character and board[4] == character and board[6] == character:
+            print(f"{player} Won!")
+            return True
+    
+    def is_board_full(self, board):
+        count = 0
+        for item in board:
+            if item == "O" or item == "X":
+                count += 1
+            else:
+                count = count
+        if count >= 9:
+            return True
+        else: 
+            return False
