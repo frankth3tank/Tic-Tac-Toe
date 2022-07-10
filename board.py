aa54d1eb3239920ac71d7fda939116cc13955431
Francisquet Vargas <fvs00725@gmail.com>
2022-07-10 14:02:20 -0500

    added implementations in board file and player file

--- board.py
+++ board.py
@@ -1,2 +1,18 @@
 class Board:
-    pass
+    
+    def __init__(self):
+        self.board = []
+        for index in range(9):
+            self.board.append(index + 1)
+     
+    def show_board(self):
+        steps = 2
+        for index in range(len(self.board)):
+            if index > 0 and index % steps == 0:
+                print(self.board[index], end="\n")
+                steps += 3
+            else:
+                print(self.board[index], end=" ")
+        
+    def play(self, character, choice):
+        self.board[choice - 1] = character
--- player.py
+++ player.py
@@ -1,2 +1,26 @@
 class Player:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def welcome_player(self):
+        print(f"Welcome {self.name} to Tic-Tac-Toe!")
+        
+    def player_choose_character(self):
+        choice = input("What character would you be playing with 'X' or 'O': ").upper()
+        if choice == "X" or choice == "O":
+            return choice
+        else:
+            print("Invalid Character, choose between 'X' or 'O'")
+            return self.player_choose_character()
+        
+    def player_choice(self):
+        choice = int(input("What position would you like to play?: "))
+        if choice < 0 or choice > 9:
+            print("Please Enter a number between 1 and 9")
+            return self.player_choice()
+        else:
+            return choice 
+    
+        
+    
